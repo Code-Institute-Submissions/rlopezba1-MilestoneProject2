bf39e7d8196990d15d52470ce1d8e4565f60497d
rlopezba1 <rubenlopezb@gmail.com>
2021-02-06 16:15:21 +0000

    Finished code game and fixing bugs

--- script.js
+++ script.js
@@ -1,21 +1,71 @@
 const cards = document.querySelectorAll('.memory-card');
 
 let hasFlippedCard = false;
+let lockBoard = false;
 let firstCard, secondCard;
 
-function flipCard(){
+function flipCard() {
+    if(lockBoard) return;
+    if(this === firstCard) return;
+
     this.classList.toggle('flip');
 
     if(!hasFlippedCard) {
         // first click
         hasFlippedCard = true;
         firstCard = this;
-    }else{
-        // second click
-        hasFlippedCard = false;
-        secondCard = this;
+    
+        return;
     }
+        // second click
+        
+        secondCard = this;
+
+       checkForMatch();
+    
 }
+
+
+function checkForMatch(){
+     // do cards match?
+     let isMatch = firstCard.dataset.starWars === secondCard.dataset.starWars;
+
+     isMatch ? disableCards() : unFlipCards();
+        
+}
+
+function disableCards(){
+    // it's a match!!
+    firstCard.removeEventListener('click', flipCard);
+    secondCard.removeEventListener('click', flipCard);
+
+    resetBoard();
+}
+
+function unFlipCards(){
+    lockBoard = true;
+
+    // not a match!!
+    setTimeout(() => {
+                firstCard.classList.remove('flip');
+                secondCard.classList.remove('flip');
+
+                resetBoard();
+            }, 1500);
+}
+
+function resetBoard(){
+    [hasFlippedCard, lockBoard] = [false, false];
+    [firstCard, secondCard] = [null, null];
+}
+
+(function shuffle() {
+    cards.forEach(card => {
+        let randomPos = Math.floor(Math.random() * 12);
+        card.style.order = randomPos;
+    })
+})();
+
 
 cards.forEach(card => card.addEventListener('click', flipCard));
 
